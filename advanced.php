622bdad1cdcee4ade7c4739ea70dac0b95fa0534
laurevaantonia <l.schulze.brockhausen@gmail.com>
2021-04-20 15:06:45 +0200

    added files

--- advanced.php
+++ advanced.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
-
 <header>
  <?php
         include 'nav.php';
@@ -27,34 +27,35 @@
                          return $celcius;
                      }
                      if (isset($_POST["submit"])) {
-                        $celcius = celcius ($_POST["temp"]);
-                          echo '<div class="fahreneheitt"> It is ' .floor($celcius). '°C
-                         </div>';
+                             $celcius = celcius ($_POST["temp"]);
                          switch ($celcius) {
                              case ($celcius<6);
-                             echo "Very cold";
+                             echo '<span class="txt"> 🥶 </br>Very cold </span>';
                              break;
                               case ($celcius<10);
-                             echo "cold";
+                             echo '<span class="txt"> ☃️ </br> cold </span>';
                              break;
                               case ($celcius<15);
-                             echo "pleasant";
+                             echo '<span class="txt"> 🥰 </br> pleasant </span>';
                              break;
                               case ($celcius<21);
-                             echo "warm";
+                             echo '<span class="txt"> 🥵 </br> warm </span>';
                              break;
                               case ($celcius>=21);
-                             echo "hot";
+                             echo '<span class="txt"> 🔥 </br> hot </span>';
                              break;
                              default : echo "something went wrong 🤡";
+                          echo '<div class="fahreneheitt"> It is ' .floor($celcius). '°C
+                         </div>';
                          }
                         }
-
-                    
-
-
                          ?>
     </div>
  </div>
 </body>
+<footer>
+     <?php
+        include 'footer.php';
+        ?>
+</footer>
 </html>
---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="d-flex flex-column justify-content-center align-items-center py-5">
+    <a href="advanced.php" class="text-dark text-decoration-none"><h5> Lauras Fahrheit Rechner &#174;</h5></a>
+    <p>All rights reserved</p>
+</div>
